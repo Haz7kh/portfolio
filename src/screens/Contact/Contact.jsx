17091be514081e3e89698ff605debc1161f1b3ed
khaled Almelhem <90411108+Haz7kh@users.noreply.github.com>
2023-05-28 16:41:53 +0200

    contact page

---
+++ src/screens/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <div id="contact" className="contact-container">
+      <p>Khaled Almelhem</p>
+      <p>Haninge, Stockholm</p>
+      <p>+46 (0)762 499 610</p>
+      <p>kh.almelhem@gmail.com</p>
+    </div>
+  );
+};
+
+export default Contact;
--- src/screens/index.js
+++ src/screens/index.js
@@ -3,5 +3,6 @@ import Landing from "./landing/Landing";
 import AboutMe from "./aboutMe/AboutMe";
 import Technologies from "./Technologies/Technologies";
 import Portfolio from "./portfolio/Portfolio";
+import Contact from "./Contact/Contact";
 
-export { Navbar, Landing, AboutMe, Technologies, Portfolio };
+export { Navbar, Landing, AboutMe, Technologies, Portfolio, Contact };
