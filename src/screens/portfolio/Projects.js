63b49ec1b487fba7e7371c0f9a969491c016aff3
khaled Almelhem <90411108+Haz7kh@users.noreply.github.com>
2023-06-23 12:41:33 +0200

    added some projects

--- src/screens/portfolio/Projects.js
+++ src/screens/portfolio/Projects.js
@@ -1,4 +1,5 @@
 import questions from "./proImgs/questions.png";
+import proj2 from "./proImgs/proj2.png";
 
 export const projects = [
   {
@@ -7,5 +8,23 @@ export const projects = [
       "An applications that make Learning and study more fun and digital  ",
     image: questions,
     url: "https://github.com/Haz7kh/quiz-learn ",
+  },
+  {
+    title: "Restaurant",
+    description: "A restaurant website  ",
+    image: proj2,
+    url: "https://haz-backroad-app.netlify.app/ ",
+  },
+  {
+    title: "Restaurant",
+    description: "A restaurant website  ",
+    image: proj2,
+    url: "https://haz-backroad-app.netlify.app/ ",
+  },
+  {
+    title: "Restaurant",
+    description: "A restaurant website  ",
+    image: proj2,
+    url: "https://haz-backroad-app.netlify.app/ ",
   },
 ];
